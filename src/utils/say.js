bf624279f9586ce497c2e1b964ed3ff37f62aad9
zwx <764454754@qq.com>
2021-05-20 17:27:36 +0800

    导入react

--- src/utils/say.js
+++ src/utils/say.js
@@ -2,4 +2,11 @@ function show() {
   window.document.getElementById('app').innerText = "潘师傅过人就像过清晨的马路";
 }
 
-module.exports = show;
+const say = () => {
+  alert('你真帅');
+};
+
+module.exports = {
+  show,
+  say
+};

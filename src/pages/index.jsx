3d9da3c4a5ba30dce37205c343dad29471fd8902
张文潇 <764454754@qq.com>
2021-06-23 16:48:30 +0800

    ???

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import { Button } from 'antd';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,10 +8,26 @@ class App extends React.Component {
     this.state = {
     };
   }
+  saveFormData = (dataType) =>{
+		return (event)=>{
+			console.log(event)
+			this.setState({[dataType]:event.target.value})
+		}
+	}
+	handleSubmit = (e) =>{
+		e.preventDefault()//阻止表单提交
+		const {username,password} = this.state;
+		alert(`用户名：${username},密码：${password}`);
+	}
 
   render() {
     return (<>
-      <button class='btn'>SB</button>
+      <button class='btn'>Button</button>
+      <form onSubmit={this.handleSubmit}>
+        用户名:<input onChange={this.saveFormData('username')} type="text" name="username"/>
+        密码:<input onChange={this.saveFormData('password')} type="text" name="password"/>
+        <button>登录</button>
+      </form>
     </>);
   }
 }

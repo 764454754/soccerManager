9d59e29fba65e6f65bf2454b85713606e39f336c
张文潇 <764454754@qq.com>
2021-06-28 10:37:46 +0800

    eslint规则配置

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,33 +3,49 @@ import './index.css';
 import { Button } from 'antd';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  saveFormData = (dataType) =>{
-		return (event)=>{
-			console.log(event)
-			this.setState({[dataType]:event.target.value})
-		}
-	}
-	handleSubmit = (e) =>{
-		e.preventDefault()//阻止表单提交
-		const {username,password} = this.state;
-		alert(`用户名：${username},密码：${password}`);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-  render() {
-    return (<>
-      <button class='btn'>Button</button>
-      <form onSubmit={this.handleSubmit}>
-        用户名:<input onChange={this.saveFormData('username')} type="text" name="username"/>
-        密码:<input onChange={this.saveFormData('password')} type="text" name="password"/>
-        <button>登录</button>
-      </form>
-    </>);
-  }
+    // saveFormData = (data) => {
+    //     return (event) => {
+    //         this.setState({ [data]: event.target.value });
+    //     };
+    // }
+    saveFormData(data) {
+        return (event) => {
+            this.setState({ [data]: event.target.value });
+        };
+    }
+
+    handleSubmit(e) {
+        // 阻止表单提交
+        e.preventDefault();
+        const { username, password } = this.state;
+
+        alert(`用户名：${username},密码：${password}`);
+    }
+
+    // handleSubmit = (e) => {
+    //     // 阻止表单提交
+    //     e.preventDefault();
+    //     const { username, password } = this.state;
+
+    //     alert(`用户名：${username},密码：${password}`);
+    // }
+
+    render() {
+        return <>
+            <button class='btn'>Button</button>
+            <form onSubmit={this.handleSubmit}>
+                用户名:<input onChange={this.saveFormData('username')} type='text' name='username'/>
+                密码:<input onChange={this.saveFormData('password')} type='text' name='password'/>
+                <button>登录</button>
+            </form>
+        </>;
+    }
 }
 
 export default App;
